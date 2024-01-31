a8342f9569db2d4685cf9d2674e0611bf205506a
ral1243 <78623785+ral1243@users.noreply.github.com>
2024-01-31 15:14:13 +0200

    faw

--- scripts/dumb1.js
+++ scripts/dumb1.js
@@ -1,4 +1,5 @@
 Events.on(BlockDestroyEvent, event => {
+  Log.info("faghkujbvlfhilv")
     if(event.tile.build.block == Blocks.thoriumReactor){
       Vars.ui.hudfrag.showToast("reactor down");
     }
