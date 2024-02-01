0441277a711e59f54dd80540784e614a65e81cbd
ral1243 <78623785+ral1243@users.noreply.github.com>
2024-02-01 12:08:19 +0200

    dum stuff

--- scripts/dumb1.js
+++ scripts/dumb1.js
@@ -1,7 +1,11 @@
 Events.on(BlockDestroyEvent, event => {
-  Log.info("faghkujbvlfhilv")
     if(event.tile.build.block == Blocks.thoriumReactor){
       Vars.ui.hudfrag.showToast("reactor down");
     }
   })
- 
+  Events.on(BlockDestroyEvent, event => {
+      if(event.tile.build.block == Blocks.impactReactor){
+        Vars.ui.hudfrag.showToast("impact down");
+      }
+    })
+    //Weathers.rain.create(100, 600)
